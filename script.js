338d8345898611f929d8ce6229be629c43afc398
Hea092024 <Hea092024@gmail.com>
2024-12-02 12:18:39 +0100

    updated js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-// document.getElementById("count").innerText = 5;
-let countEl = document.getElementById("count-el")
+// document.getElementById("count-el").innerText = 5
 
-console.log(countEl)
+// change the count-el in the HTML to reflect the new count
 
-let count = 0
+// camelCase
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
+
 function increment() {
-  count = count + 1
-  countEl.innerText = count
-  console.log("The button was clicked");
-
+  count = count + 1;
+  countEl.innerText = count;
 }
