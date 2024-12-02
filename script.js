5e07e8fde50836915b75780d8b54f16648b399c4
Hea092024 <Hea092024@gmail.com>
2024-12-02 11:41:02 +0100

    .

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // document.getElementById("count").innerText = 5;
 let countEl = document.getElementById("count-el")
 
+console.log(countEl)
 
 let count = 0
 function increment() {
   count = count + 1
+  countEl.innerText = count
   console.log("The button was clicked");
 
 }
